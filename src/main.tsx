10d6f6f4a694b6dcd5fbf65d38072836955678e8
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2023-09-16 15:33:43 +0100

    debug4

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
 const queryClient = new QueryClient();
